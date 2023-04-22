0b580cbcf5c2f65479e26c8e511f63848e708108
GonzaloAllenPerez444 <GonzaloAllenPerez444@gmail.com>
2023-04-22 09:11:29 -0700

    Now the form can be submitted!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import enter from './input';
+import Input from './input';
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
 
     <div className="App">
       <header className="App-header">
-        <enter />
+        <Input />
       </header>
     </div>
   );
--- src/input.js
+++ src/input.js
@@ -1,19 +1,36 @@
-const enter = () =>{
+import {useState} from 'react'
+
+const Input = () =>{
+
+    const [place, setPlace] = useState('');
 
     const handleClick = () => {
         const textInput = document.getElementById('location').value;
         console.log('User input:', textInput);
       };
+
+      function runScriptWithInput(event){
+        console.log("DO API STUFF EVENTUALLY, Argument is " + event);
+      }
+
+      function handleSubmit(event) {
+        event.preventDefault();
+        console.log("form submitted")
+        
+
+        // Call function to run script with input data
+        runScriptWithInput(place);
+      }
       
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="location">Enter location:</label>
-          <input type="text" id="location" />
-          <button id="submit" type="button" onClick={handleClick}>
+          <input type="text" id="location" value={place} onChange={(event) => setPlace(event.target.value)} />
+          <button id="submit" type="submit" onClick={handleClick}>
             Submit
           </button>
         </form>
     );
 }
 
-export default input
+export default Input
