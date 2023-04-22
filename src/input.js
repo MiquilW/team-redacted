3f9a46ef9253f4eb459d24ecab832a1cb4a47f0e
Ryan Gertz <ryan.gertz1@gmail.com>
2023-04-22 16:22:55 -0700

    made queue class

--- src/input.js
+++ src/input.js
@@ -3,7 +3,58 @@ import {getAnimal} from './NatureApi.mjs'
 import Species from './Species.js';
 
 
+
 const Input = () =>{
+
+  class Queue{
+    constructor(cap){
+          this.capacity = cap;
+          this.items = Array(cap).fill(null);   //array for queue
+          this.num_items = 0                 // number of items in stack
+          this.front = 0                      //front index of queue (items removed from front)
+          this.rear =0
+    }
+  
+    is_empty(){
+      if (this.num_items === 0){
+        return true}
+        return false 
+    }
+  
+    is_full(){
+      if(this.num_items === this.capacity){
+        return true 
+      } return false
+    }
+  
+      enqueue(item){
+        if (this.is_full() === true){
+          throw new Error("queue is full")
+        }
+  
+        this.items[this.rear] = item
+        this.num_items++
+        this.rear = (this.rear +1) % this.capacity
+  
+      }
+  
+      dequeue(){
+          if (this.is_empty() === true){
+            throw new Error("queue is empty")
+          }
+          let val = this.items[this.front]
+          this.num_items--
+          this.front = (this.front +1) % this.capacity
+          return val
+      }
+    
+  }
+    let q = new Queue(3)
+    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
+    
+
 
     const [place, setPlace] = useState('');
     const [animal, setAnimal] = useState('');
