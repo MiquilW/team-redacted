cdc7c846f1fc7c46b5205604d416f1204f76fada
Ryan Gertz <ryan.gertz1@gmail.com>
2023-04-22 22:31:52 -0700

    default pfp

--- src/Species.js
+++ src/Species.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Species = props => {
   return (
     <div className='species'>
-        <img className='pic' src={require('./pocketmouse.jpg')} alt='lol'/>
+        <img className='pic' src={require('./pfp_1.png')} alt='lol'/>
         <h2 className='name'>{props.name}</h2>
     </div>
   )
